952bd08ce66cc8aa2430ada11dffc909ed7a27ea
Katrin <ekaterina.omg@gmail.com>
2013-08-08 22:00:06 +0300

    init commit

---
+++ TestWebCrowler/MainWindow.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace TestWebCrowler
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        String Rstring;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            WebRequest myWebRequest;
+            WebResponse myWebResponse;
+            String URL = textBox1.Text;
+
+            myWebRequest = WebRequest.Create(URL);
+            myWebResponse = myWebRequest.GetResponse();//Returns a response from an Internet resource
+
+            Stream streamResponse = myWebResponse.GetResponseStream();//return the data stream from the internet
+            //and save it in the stream
+
+            StreamReader sreader = new StreamReader(streamResponse);//reads the data stream
+            Rstring = sreader.ReadToEnd();//reads it to the end
+            var Links = Index(Rstring);//gets the links only
+
+            textBox2.Text = Rstring;
+            textBox3.Text = Links;
+            streamResponse.Close();
+            sreader.Close();
+            myWebResponse.Close();
+        }
+
+        
+
+        public ISet<string> GetNewLinks(string content)
+        {
+            Regex regexLink = new Regex("(?<=<a\\s*?href=(?:'|\"))[^'\"]*?(?=(?:'|\"))");
+
+            ISet<string> newLinks = new HashSet<string>();
+            foreach (var match in regexLink.Matches(content))
+            {
+                if (!newLinks.Contains(match.ToString()))
+                    newLinks.Add(match.ToString());
+            }
+
+            return newLinks;
+        }
+
+        public string Index(string content)
+        {
+            XNamespace ns = "http://www.sitemaps.org/schemas/sitemap/0.9";
+            var items = GetNewLinks(content);
+            var sitemap = new XDocument(
+                new XDeclaration("1.0", "utf-8", "yes"),
+                new XElement(ns + "urlset",
+                    from i in items
+                    select
+                        //Add ns to every element.
+                    new XElement(ns + "url",
+                      new XElement(ns + "loc", i)
+                      )
+                    )
+                  );
+            return sitemap.ToString();
+        }
+    }
+}
